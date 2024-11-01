215e49e99d7e922adca7bff268bd578e4da248e5
hzknb <huangzhaokang111@126.com>
2024-11-01 17:28:02 +0800

    scanner1

---
+++ 01_scanner/main.c
@@ -0,0 +1,47 @@
+#include "defs.h"
+#define extern_
+#include "data.h"
+#undef extern_
+#include "decl.h"
+#include <errno.h>
+
+static void usage(char *prog){
+    fprintf(stderr, "Usage: %s infile\n", prog);
+    exit(1);
+}
+
+
+static void init(){
+    Line = 1;
+    Putback = '\n';
+}
+
+char *tokenstr[] = {"+", "-", "*", "/", "intlit"};
+
+static void scanfile(){
+    struct token T;
+
+    while (scan(&T)){
+        printf("Token %s", tokenstr[T.token]);
+        if (T.token == T_INTLIT)
+            printf(", value %d", T.intvalue);
+        printf("\n");
+    }
+}
+
+void main(int argc, char *argv[]){
+    if (argc != 2)
+        usage(argv[0]);
+
+    init();
+
+    if ((Infile = fopen(argv[1], "r")) == NULL){
+        fprintf(stderr, "Unable to open %s: %s\n", argv[1], strerror(errno));
+        exit(1);
+    }
+
+    scanfile();
+    exit(0);
+
+}
+
